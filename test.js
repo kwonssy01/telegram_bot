a5021240d2407dd708b0ebe5faf49c1ec55a6305
Ubuntu <ubuntu@ip-172-31-6-17.ap-northeast-2.compute.internal>
2016-04-28 13:10:09 +0000

    init commit

---
+++ test.js
@@ -0,0 +1,23 @@
+var TelegramBot = require('node-telegram-bot-api');
+
+var token = '212992113:AAER5F7gY_0rN18XLLHiQu0tn_QLOYrgH54';
+// Setup polling way
+var bot = new TelegramBot(token, {polling: true});
+
+// Matches /echo [whatever]
+bot.onText(/\/echo (.+)/, function (msg, match) {
+  console.log('bbb');
+  var fromId = msg.from.id;
+  var resp = match[1];
+  bot.sendMessage(fromId, resp);
+});
+
+// Any kind of message
+bot.on('message', function (msg) {
+  var chatId = msg.chat.id;
+  console.log(chatId);
+  // photo can be: a file path, a stream or a Telegram file_id
+  //var photo = 'cats.png';
+  bot.sendMessage(chatId, 'hi');
+  //bot.sendPhoto(chatId, photo, {caption: 'Lovely kittens'});
+});
