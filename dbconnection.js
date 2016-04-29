83871349da4f7a6583a307860f42077f781814be
kwonssy02 <kwonssy02@gmail.com>
2016-04-30 02:50:41 +0900

    Update dbconnection.js

--- dbconnection.js
+++ dbconnection.js
@@ -1,9 +1,9 @@
 module.exports = function() { 
 	this.connection = require('mysql').createConnection({
-                host : "awsdbinstance.ctgrun1qbjn8.ap-northeast-2.rds.amazonaws.com",
+                host : "example.com",
                 port : 3306,
                 user : "master",
-                password : "master12",
-                database : "someTar"
+                password : "password",
+                database : "database"
 	});
 };
